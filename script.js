4364d633ec08bf0915dcabe2b352062e23a10cc8
LMNTRXUZ <sweetmedicine07@gmail.com>
2025-04-11 02:36:34 +0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const burgerMenu = document.querySelector(".header__burger-menu");
-const menu = document.querySelector(".header__menu");
-
-burgerMenu.addEventListener("click", () => {
-  menu.classList.toggle("active"); 
-});
